8b5f0bfd95ed69cc7409679c51a4950daa6025ba
Josh Bernstein <school.josh@gmail.com>
2018-05-24 23:52:01 -0600

    Setting up production keys

---
+++ config/prod.js
@@ -0,0 +1,18 @@
+//prod.js - production keys
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY
+};
+
+/*
+module.exports = {
+  googleClientID:
+    '643909691694-6tc0hmsjkdf30tgqhe0g54nf6fcr3ies.apps.googleusercontent.com',
+  googleClientSecret: 'O5fEUgX7O5uNzMUN2USwIJD9',
+  mongoURI: 'mongodb://admin:Viv&]u?aN%E9(fUdGbKmf7Bg74D7dRod@ds233500.mlab.com:33500/emaily_prod',
+  cookieKey: 'poquhjdfhoasdufh890182uansdf81nasdfu839jsdf893j90pjsdfka0s9d8fj2398ijidj83jdkj8dkjdkj'
+};
+
+*/
