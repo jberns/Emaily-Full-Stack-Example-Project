9b97b9959b4ac281b785d3f0a99bf245a4d3e989
Josh Bernstein <school.josh@gmail.com>
2018-05-25 00:04:33 -0600

    Reset keys and Fixed prod.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 // keys.js - Determine set of credentials to return
 if (process.env.NODE_ENV === 'production') {
   //In production - return prod keys
-  module.exports = require('.prod');
+  module.exports = require('./prod');
 } else {
   //In development
   module.exports = require('./dev');
