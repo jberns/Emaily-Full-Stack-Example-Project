fe337904a1b3a3c64252fb2e748752f894e4c826
Josh Bernstein <school.josh@gmail.com>
2018-05-30 20:43:13 -0600

    Added middleware for logged out routes

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-//Route Handler
+if (process.env.NODE_ENV === 'production') {
+  //Express to serve up production assets (main.css and main.js)
+  app.use(express.static('client/build'));
+  //Express to serve up index.html
+  const path = require('path');
+  app.get('*', () => (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+  });
+}
+
+//Route Handler for port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
